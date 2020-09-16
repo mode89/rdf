fb3beeecf9cb592c987f8ce9b3222e713e5aea14
Andrey Krainyak <mode89@mail.ru>
2020-09-17 00:51:44 +0800

    Make sure that ui.window is QMainWindow
    That is what we expect to get from the window.ui file.

--- ui.py
+++ ui.py
@@ -1,5 +1,6 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
 import sys
 
 class UI:
@@ -7,6 +8,7 @@ class UI:
     def __init__(self):
         self.app = QApplication(sys.argv)
         self.window = uic.loadUi("window.ui")
+        assert isinstance(self.window, QMainWindow)
         self.window.show()
 
     def run(self):
