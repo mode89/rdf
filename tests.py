6f70fa7d26cc38629ffdf8ee6e3e996680c5158f
Andrey Krainyak <mode89@mail.ru>
2020-09-16 13:48:21 +0800

    Instantiate widget inside setUp()
    Eliminate duplicated initialization inside tests.

--- tests.py
+++ tests.py
@@ -3,18 +3,18 @@ import unittest
 
 class TestWidget(unittest.TestCase):
 
+    def setUp(self):
+        self.widget = Widget()
+
     def test_widget_is_visible(self):
-        widget = Widget()
-        self.assertTrue(widget.is_visible())
+        self.assertTrue(self.widget.is_visible())
 
     def test_initial_stage_is_red(self):
-        widget = Widget()
-        self.assertEqual(widget.stage, Stage.RED)
+        self.assertEqual(self.widget.stage, Stage.RED)
 
     def test_advance_from_red_to_green(self):
-        widget = Widget()
-        widget.advance_stage()
-        self.assertEqual(widget.stage, Stage.GREEN)
+        self.widget.advance_stage()
+        self.assertEqual(self.widget.stage, Stage.GREEN)
 
 if __name__ == "__main__":
     unittest.main()
