4ea45a45534161488aa18d55f3ec7e9693470813
Andrey Krainyak <mode89@mail.ru>
2020-09-16 13:11:56 +0800

    Test if widget is visible
    After widget was created it should be visible.

---
+++ tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestWidget(unittest.TestCase):
+
+    def test_show_widget(self):
+        widget = Widget()
+        self.assertTrue(widget.is_visible())
+
+if __name__ == "__main__":
+    unittest.main()
