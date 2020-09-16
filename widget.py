7862f07da14339c17ab9e6927b87a0f066555b40
Andrey Krainyak <mode89@mail.ru>
2020-09-16 13:38:14 +0800

    Transition from RED to GREEN stage
    GREEN stage should follow the RED stage.

--- widget.py
+++ widget.py
@@ -1,11 +1,19 @@
 class Widget:
 
+    def __init__(self):
+        self.stage = Stage.RED
+
     def is_visible(self):
         return True
 
     def get_stage(self):
-        return Stage.RED
+        return self.stage
+
+    def advance(self):
+        if self.stage == Stage.RED:
+            self.stage = Stage.GREEN
 
 class Stage:
 
     RED = 1
+    GREEN = 2
