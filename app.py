3e4c5e6f61813866a2ae811136c20ee05543700b
Andrey Krainyak <mode89@mail.ru>
2020-09-16 15:46:05 +0800

    Run the app
    Perform manual testing.

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ class App:
     def __init__(self):
         self.stage = Stage.RED
 
+    def run(self):
+        pass
+
     def advance_stage(self):
         if self.stage == Stage.RED:
             self.stage = Stage.GREEN
@@ -14,3 +17,7 @@ class App:
             self.stage = Stage.REFACTOR
         elif self.stage == Stage.REFACTOR:
             self.stage = Stage.RED
+
+if __name__ == "__main__":
+    app = App()
+    app.run()
